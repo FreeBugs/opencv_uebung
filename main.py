a8e7a3dd27ea0844388ddfa35855776333539f9e
Daniel Arnold <daniel.arnold@senticon.de>
2022-11-18 17:08:02 +0100

    Zeichnet Rechtecke um Gesichter

--- main.py
+++ main.py
@@ -9,6 +9,20 @@ if __name__ == '__main__':
     if img is None:
         print('Bild nicht gefunden')
 
+    face_detector = cv2.CascadeClassifier('haarcascade_fullbody.xml')
+    if face_detector.empty():
+        print('XML-Datei nicht geladen.')
+        exit(1)
+
+    faces = face_detector.detectMultiScale(img, 1.1, 4)
+
+    for face in faces:
+        x, y, w, h = face
+        top_left = (x, y)
+        bottom_right = (x + w, y + h)
+        rect_color = (0, 255, 255)  # B, G, R
+        cv2.rectangle(img, top_left, bottom_right, rect_color, thickness=3)
+
     # Show image
     cv2.imshow('image', img)
 
